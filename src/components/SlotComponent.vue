f26b95a130df46e2a19f6342921f0d2bf719634a
Your Name <1419746078@qq.com>
2020-03-18 22:37:36 +0800

    slot&ref

---
+++ src/components/SlotComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <slot name="first"></slot>
+        <slot></slot>
+        <slot name="last"></slot>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/SlotPractice.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+    <router-link to="/">Home</router-link>
+    <SlotComponent>
+      <template v-slot:first>
+        <h1>来自父组件的具名(first)分发内容</h1>
+      </template>
+      <template v-slot:default>
+        <input type="button" @click="getElem" value='点击控制台打印DOM元素' />
+        <h1>来自父组件分发的内容(无具名,子组件没slot无具名标签我会被抛弃)</h1>
+      </template>
+      <template v-slot:last>
+        <h1 ref="msg">来自父组件的具名(last)分发内容</h1>
+      </template>
+    </SlotComponent>
+  </div>
+</template>
+<script>
+
+import SlotComponent from '@/components/SlotComponent'
+
+export default {
+  components: {
+    SlotComponent
+  },
+  methods: {
+    getElem () {
+      console.log(this.$refs.msg)
+    }
+  }
+}
+</script>
